e8eff2d8d4e2cf377a9c28ca60e0f5a3e1d2cb8b
KlaamZie <enzo.viry@gmail.com>
2021-04-13 10:43:01 +0200

    fix: UI styles

--- styles/UI/Texts.style.js
+++ styles/UI/Texts.style.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
+import breakpoints from './breakpoints';
+
 export const Title = styled.h1`
-    font-size: 40px;
+    font-size: 30px;
     font-weight: normal;
     color: ${(props) => props.theme.color.green.default};
+
+    @media all and (${breakpoints.device.s}) {
+        font-size: 40px;
+    }
 `;
 
 export const Subtitle = styled.h2`
---
+++ styles/breakpoints.js
@@ -0,0 +1,13 @@
+const size = {
+    s: '768px',
+    m: '1440px',
+    lg: '1920px'
+};
+
+const device = {
+    s: `min-width: ${size.s}`,
+    m: `min-width: ${size.m}`,
+    lg: `min-width: ${size.lg}`
+};
+
+export default { size, device };

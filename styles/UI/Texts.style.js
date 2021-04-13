54ce92b7fc12fcde551aaef935fbb73f5451b919
KlaamZie <enzo.viry@gmail.com>
2021-04-13 10:45:37 +0200

    fix: UI styles

--- styles/UI/Texts.style.js
+++ styles/UI/Texts.style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import breakpoints from './breakpoints';
+import breakpoints from '../breakpoints';
 
 export const Title = styled.h1`
     font-size: 30px;

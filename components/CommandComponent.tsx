3bc0360e8b39b05dbf2929d31f9dc38c760588fb
enzo-nabert <enzo.nabert@etu.umontpellier.fr>
2021-05-25 16:31:18 +0200

    test: leatsy class

--- components/CommandComponent.tsx
+++ components/CommandComponent.tsx
@@ -18,18 +18,8 @@ export default function CommandComponent({ data }) {
         console.log(leatsy);
         leatsy.classList.remove('fb_customer_chat_bounce_out_v2');
         leatsy.classList.add('fb_customer_chat_bounce_in_v2');
-        leatsy.style.padding = '0px';
-        leatsy.style.position = 'fixed';
-        leatsy.style.zIndex = '2147483646';
-        leatsy.style.borderRadius = '16px';
-        leatsy.style.top = 'auto';
-        leatsy.style.width = '399px';
-        leatsy.style.background = 'none';
-        leatsy.style.bottom = '85px';
-        leatsy.style.maxHeight = 'calc(100 % - 80px)';
-        leatsy.style.right = '4px';
-        leatsy.style.minHeight = '360px';
-        leatsy.style.height = '509px';
+        // leatsy.style.maxHeight = 'calc(100 % - 80px)';
+        // leatsy.style.minHeight = '360px';
 
         console.log(showDialog);
         setShowDialog(true);

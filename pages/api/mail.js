02493aab01236267ce8070e9ec0724699323b52f
Enzo Viry <enzoviry@MacBook-Pro-de-Enzo.local>
2022-08-12 17:13:07 +0200

    fix contact form

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,44 +1,61 @@
-import sgMail from '@sendgrid/mail';
-import nodemailer from 'nodemailer';
+// import sgMail from '@sendgrid/mail';
+// import nodemailer from 'nodemailer';
+// import { ApiClient, SendSmtpEmail, TransactionalEmailsApi } from 'sib-api-v3-sdk';
+
+import axios from 'axios';
 
 export default (req, res) => {
-    let transporter = nodemailer.createTransport({
-        host: 'smtp-relay.sendinblue.com',
-        port: 587,
-        secure: false, // upgrade later with STARTTLS
-        auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS
-        }
-    });
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    var SibApiV3Sdk = require('sib-api-v3-sdk');
+    var defaultClient = SibApiV3Sdk.ApiClient.instance;
 
-    const msg = {
-        to: `Site Web <${process.env.MAIL_USER}>`, // Change to your recipient
-        from: `Site Web <${process.env.MAIL_USER}>`, // Change to your verified sender
-        replyTo: req.body.email,
-        subject: `Message de ${req.body.name} depuis le site web`,
-        text: `
-        - Nom : ${req.body.name}
-        - Mail : ${req.body.email}
-        - Téléphone : ${req.body.tel || 'Non renseigné'}
-        - Type de client : ${req.body.clientType || 'Non renseigné'}
-        - Question : ${req.body.question || 'Non renseigné'}
-        `,
-        html: `
-        <div>
-        <p>Nom : ${req.body.name}</p>
-        <p>Mail : ${req.body.email}</p>
-        <p>Téléphone : ${req.body.tel || 'Non renseigné'}</p>
-        <p>Type de client : ${req.body.clientType || 'Non renseigné'}</p>
-        <p>Question : ${req.body.question || 'Non renseigné'}</p>
-        </div>`
+    var apiKey = defaultClient.authentications['api-key'];
+    apiKey.apiKey = process.env.MAILER_API;
+
+    var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+    var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+    sendSmtpEmail = {
+        to: [{ name: 'Site Web', email: process.env.MAIL_USER }],
+        replyTo: { name: req.body.name, email: req.body.email },
+        templateId: 2,
+        params: {
+            name: req.body.name,
+            mail: req.body.email,
+            phone: req.body.tel || 'Non renseigné',
+            client: req.body.clientType || 'Non renseigné',
+            message: req.body.question || 'Non renseigné'
+        },
+        // textContent: `
+        // - Nom : ${req.body.name}
+        // - Mail : ${req.body.email}
+        // - Téléphone : ${req.body.tel || 'Non renseigné'}
+        // - Type de client : ${req.body.clientType || 'Non renseigné'}
+        // - Question : ${req.body.question || 'Non renseigné'}
+        // `,
+        // htmlContent: `
+        // <div>
+        // <p>Nom : ${req.body.name}</p>
+        // <p>Mail : ${req.body.email}</p>
+        // <p>Téléphone : ${req.body.tel || 'Non renseigné'}</p>
+        // <p>Type de client : ${req.body.clientType || 'Non renseigné'}</p>
+        // <p>Question : ${req.body.question || 'Non renseigné'}</p>
+        // </div>`
+        headers: {
+            accept: 'application/json',
+            'content-type': 'application/json',
+            'api-key': process.env.MAILER_API
+        }
     };
 
-    try {
-        transporter.sendMail(msg);
-        res.json({ text: 'Email sent' });
-    } catch (err) {
-        console.log(err);
-        res.json({ err });
-    }
+    apiInstance.sendTransacEmail(sendSmtpEmail).then(
+        function (data) {
+            res.json({ text: 'Email sent' });
+        },
+        function (error) {
+            console.log(error);
+            res.json({ error });
+        }
+    );
 };

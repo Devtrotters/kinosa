c75b6b3cec42f01080a2e1c75d5e9674f6f5edbb
Enzo Viry <enzo.viry@gmail.com>
2022-11-01 15:54:59 +0100

    fix this shit

--- pages/api/getReviews.js
+++ pages/api/getReviews.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default async (req, res) => {
     const config = {
         method: 'get',
-        url: `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJg_JHs9OvthIRW6cRDRHcDWE&sensor=true&key=${process.env.GOOGLE_API_KEY}&language=fr&fields=reviews`,
+        url: `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJg_JHs9OvthIRW6cRDRHcDWE&sensor=true&key=AIzaSyCQ7GVa2SUwnNXW9Pep_oR2w_cVmleEeqM&language=fr&fields=reviews`,
         headers: {}
     };
 

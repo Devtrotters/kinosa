d5db4b88210a4f88d8d204837417c006721e1d9c
Kinosa <84857685+Kinosa@users.noreply.github.com>
2024-07-02 13:28:06 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -401,6 +401,7 @@ export default function Footer({ data, menu, reviews }) {
                 </ContactSection>
                 <PdvSection>
                     <HorairesText>Restaurant sur place ou à emporter</HorairesText>
+                    Du lundi au vendredi de 10h à 15h30
                     <PdvText href="https://goo.gl/maps/rD6LGTSCuenwou9s9">
                         90, rue Maryam Mirzakhani <br /> 34000 Montpellier
                     </PdvText>
